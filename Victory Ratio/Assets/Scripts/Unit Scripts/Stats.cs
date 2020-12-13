9f0ad5ca0a029fd997be490628d39f505141a4e6
bearinfestedwaters <bearinfestedwaters@gmail.com>
2020-12-12 20:29:55 -0500

    Prototype assets imported. Various folders and starting scripts created. Added some identifying features to the "Stats" script.

---
+++ Victory Ratio/Assets/Scripts/Unit Scripts/Stats.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    public enum UnitType
+	{
+		Sword,
+		Spear,
+		Axe,
+		Cavalry,
+		Archer
+	}
+	//Could probably do with Boolean, but if added escort missions or NPCs, may be better this way.
+	public enum Alignment
+	{
+		Player,
+		Enemy
+	}
+	[SerializeField] int count;
+	int movementSpeed;
+	[SerializeField] UnitType type;
+	public Stats(UnitType type, int count = 0)
+	{
+		this.count = count;
+		this.type = type;
+	}
+	void Start()
+	{
+
+	}
+}
