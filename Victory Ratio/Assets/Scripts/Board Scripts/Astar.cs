6cba25131660e9dc57ae1c2a63daf146f029cbbf
bearinfestedwaters <bearinfestedwaters@gmail.com>
2021-01-03 20:15:30 -0500

    UnitManager.cs- Created, keeps track of player, enemy, and NPC units. Unit.cs stores and handles unit information.

--- Victory Ratio/Assets/Scripts/Board Scripts/Astar.cs
+++ Victory Ratio/Assets/Scripts/Board Scripts/Astar.cs
@@ -21,6 +21,9 @@ public class Astar : MonoBehaviour
 	private Tile[] tiles;
 
 	[SerializeField]
+	UnitsManager unitsManager;
+
+	[SerializeField]
 	private Camera camera;
 
 	[SerializeField]
---
+++ Victory Ratio/Assets/Scripts/Unit Scripts/Unit.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Unit : MonoBehaviour
+{
+    [SerializeField]
+    int count;
+
+    int movementSpeed;
+
+    int defaultMovementSpeed = 3;
+    int cavalryMovementSpeed = 5;
+
+
+    public enum UnitType
+    {
+        Sword,
+        Spear,
+        Axe,
+        Cavalry,
+        Archer
+    }
+    //Could probably do with Boolean, but if added escort missions or NPCs, may be better this way.
+    public enum Alignment
+    {
+        Player,
+        Enemy
+    }
+
+    [SerializeField]
+    UnitType type;
+    [SerializeField]
+    Alignment alignment;
+
+    public Unit(UnitType type, int count = 0, Alignment alignment = Alignment.Player)
+    {
+        this.count = count;
+        this.type = type;
+        this.alignment = alignment;
+    }
+
+ 
+
+
+
+    void SetMovementSpeed()
+    {
+        switch (type)
+        {
+            case UnitType.Cavalry:
+                movementSpeed = cavalryMovementSpeed;
+                break;
+            default:
+                movementSpeed = defaultMovementSpeed;
+                break;
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
