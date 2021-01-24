ca3b754e101236152a91a75e4838ceeaa4b24fd8
Aaron T <bearinfestedwaters@gmail.com>
2021-01-24 18:05:03 -0500

    CanvasManager.cs Unit count display follows units. Currently behaves poorly when set to execute in editor.

--- Victory Ratio/Assets/Scripts/Managers/CanvasManager.cs
+++ Victory Ratio/Assets/Scripts/Managers/CanvasManager.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-
+//[ExecuteAlways]
 public class CanvasManager : MonoBehaviour
 {
 	[SerializeField]
@@ -13,14 +13,16 @@ public class CanvasManager : MonoBehaviour
 	GameObject allUnits;
 	Transform playerUnits, enemyUnits, npcUnits;
 
-	Dictionary<GameObject, GameObject> CountUnitPairs;
+	Dictionary<GameObject, GameObject> countUnitPairs;
 
 	List<GameObject> countInstances = new List<GameObject>();
 	
     // Start is called before the first frame update
     void Start()
     {
-		foreach(Transform child in allUnits.transform)
+		ClearCounts();
+		countUnitPairs = new Dictionary<GameObject, GameObject>();
+		foreach (Transform child in allUnits.transform)
 		{
 			if (child.CompareTag("PlayerUnits"))
 			{
@@ -37,12 +39,15 @@ public class CanvasManager : MonoBehaviour
 		}
 		SetAllUnitCounts();
     }
-
+	
     // Update is called once per frame
     void Update()
     {
-        
+		CountsFollowUnits();
     }
+	/// <summary>
+	/// 
+	/// </summary>
 	void SetAllUnitCounts()
 	{
 		foreach (Transform child in allUnits.transform)
@@ -52,19 +57,39 @@ public class CanvasManager : MonoBehaviour
 				var unitScript = grandchild.GetComponent<Unit>();
 				if (unitScript != null)
 				{
-					RenderCount(grandchild, unitScript.GetCount());
+					MakeUnitCountPair(grandchild, unitScript.GetCount());
 				}
 			}
 		}
 	}
-	void RenderCount(Transform unit, int count)
+	/// <summary>
+	/// Uses dictionary to link canvas count objects to the units,
+	/// as well as tracking each in a list.
+	/// </summary>
+	/// <param name="unit"></param>
+	/// <param name="count"></param>
+	void MakeUnitCountPair(Transform unit, int count)
 	{
 		GameObject go = Instantiate(countPrefab, transform);
 		go.transform.position = new Vector3(unit.position.x + .5f, unit.position.y + .5f, unit.position.z);
 		go.GetComponentInChildren<TextMeshProUGUI>().SetText(count.ToString());
+		countUnitPairs.Add(unit.gameObject, go);
 		countInstances.Add(go);
 
 	}
+	/// <summary>
+	/// Keeps the canvas count objects following the units even in editor mode. 
+	/// </summary>
+	void CountsFollowUnits()
+	{
+		foreach(var pair in countUnitPairs)
+		{
+			pair.Value.transform.position = pair.Key.transform.position + new Vector3(.5f, .5f, 0f);
+		}
+	}
+	/// <summary>
+	/// 
+	/// </summary>
 	void ClearCounts()
 	{
 		foreach(GameObject go in countInstances)
